d6903134c0bbc71c2329dfc32713be03d1e7ae34
Farahshabir <farah.shabir@hotmail.com>
2024-04-13 11:06:58 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,55 @@
+import inquirer from "inquirer";
+let todos = ["tea", "coffee"];
+async function createTodo(todos) {
+    do {
+        let ans = await inquirer.prompt({
+            type: "list",
+            message: "Select an operation",
+            name: "select",
+            choices: ["Add", "update", "view", "delete"],
+        });
+        if (ans.select == "Add") {
+            let addTodo = await inquirer.prompt({
+                type: "input",
+                message: "Add items in the list",
+                name: "todo",
+            });
+            todos.push(addTodo.todo);
+            todos.forEach(todo => console.log(todo));
+            // console.log(todos);
+        }
+        if (ans.select == "update") {
+            let updateTodo = await inquirer.prompt({
+                type: "list",
+                message: "update items in the list",
+                name: "todo",
+                choices: todos.map(item => item)
+            });
+            let addTodo = await inquirer.prompt({
+                type: "input",
+                message: "Add items in the list",
+                name: "todo",
+            });
+            let newTodo = todos.filter(val => val !== updateTodo.todo);
+            todos = [...newTodo, addTodo.todo];
+            console.log(todos);
+        }
+        if (ans.select == "view") {
+            console.log("*** TO DO LIST ***");
+            console.log(todos);
+            console.log("**************");
+        }
+        if (ans.select == "delete") {
+            let deleteTodo = await inquirer.prompt({
+                type: "list",
+                message: "delete items from the list",
+                name: "todo",
+                choices: todos.map(item => item)
+            });
+            let newTodo = todos.filter(val => val !== deleteTodo.todo);
+            todos = [...newTodo];
+            console.log(todos);
+        }
+    } while (true);
+}
+createTodo(todos);
